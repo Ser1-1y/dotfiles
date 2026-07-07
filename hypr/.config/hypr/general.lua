e14821551b67666be43dfb62f0c8988d6f33d010
Ser1-1y <160628372+Ser1-1y@users.noreply.github.com>
2026-07-07 21:40:32 +0300

    update

--- hypr/.config/hypr/general.lua
+++ hypr/.config/hypr/general.lua
@@ -8,13 +8,27 @@
 --## MONITORS ###
 --###############
 
+
 hl.monitor({
-    output = "",
-    mode = "highrr",
+    output = "HDMI-A-1",
+    mode = "1920x1080@74.97",
     position = "auto",
     scale = "1",
 })
 
+hl.monitor({
+   output = "eDP-1",
+   mode = "2560x1600@120.00",
+   position = "auto",
+   scale = "1",
+})
+
+-- hl.monitor({
+--     output = "eDP-1",
+--     mode = "2560x1600@60.00",
+--     position = "auto",
+--     scale = "1",
+-- })
 --##################
 --## MY PROGRAMS ###
 --##################
